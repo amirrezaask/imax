1ef1ac2c7d03c1190749e9400f895fc0e728a0d7
amirrezaask <raskarpour@gmail.com>
2020-02-18 23:51:37 +0330

    php lsp works like a charm

--- lib/modules/php.el
+++ lib/modules/php.el
@@ -1,7 +1,9 @@
 (defun module-php--install-packages ()
   (pkgmgr-install '(php-mode)))
+;; in order for php module to work as promised you need an implementation of lsp for php like intelephense
 (defun module-php-setup ()
-  (module-php--install-packages))
+  (module-php--install-packages)
+  (add-hook 'php-mode-hook 'lsp))
   
 
 (provide 'modules/php)
